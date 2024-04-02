df9a55fb0574ea9adb9a8ab5ed1e58abc431386c
Berkay Derin <zewodi@gmail.com>
2024-04-02 05:44:31 +0300

    ...

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,7 @@
-import mongoose, { Schema, Document } from "mongoose";
-import bcrypt from "bcryptjs";
-
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 
 const MONGO_URL =
   "mongodb+srv://zewodi:u52fNsMt8wefgV8Y@cluster0.bn64c0f.mongodb.net";
@@ -12,33 +11,21 @@ mongoose
   .then(() => console.log("MongoDB bağlantısı başarılı"))
   .catch((err) => console.error("MongoDB bağlantı hatası", err));
 
-interface IBook extends Document {
-  title: string;
-  author: string;
-  publicationYear: number;
-  genre: string;
-}
-
-const bookSchema = new Schema<IBook>({
+const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   publicationYear: Number,
   genre: String,
 });
 
-const Book = mongoose.model<IBook>("Book", bookSchema);
+const Book = mongoose.model("Book", bookSchema);
 
-interface IUser extends Document {
-  username: string;
-  password: string;
-}
-
-const userSchema = new Schema<IUser>({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-const User = mongoose.model<IUser>("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 const typeDefs = `#graphql
     type Book {
@@ -53,21 +40,19 @@ const typeDefs = `#graphql
         books: [Book]
         book(id: ID!): Book
     }
-
-	
-	type User {
-		id: ID!
-		username: String
-	}
+    
+    type User {
+        id: ID!
+        username: String
+    }
 
     type Mutation {
-		addBook(title: String!, author: String!, publicationYear: Int, genre: String): Book
-		updateBook(id: ID!, title: String, author: String, publicationYear: Int, genre: String): Book
-		deleteBook(id: ID!): String
-		register(username: String!, password: String!): User
-		login(username: String!, password: String!): Boolean
-	}
-
+        addBook(title: String!, author: String!, publicationYear: Int, genre: String): Book
+        updateBook(id: ID!, title: String, author: String, publicationYear: Int, genre: String): Book
+        deleteBook(id: ID!): String
+        register(username: String!, password: String!): User
+        login(username: String!, password: String!): Boolean
+    }
 `;
 
 const resolvers = {
@@ -92,13 +77,13 @@ const resolvers = {
       await Book.findByIdAndDelete(id);
       return "Silme işlemi başarılı";
     },
-    async register(_, { username, password }) {
+    register: async (_, { username, password }) => {
       const hashedPassword = await bcrypt.hash(password, 10);
       const newUser = new User({ username, password: hashedPassword });
       await newUser.save();
       return newUser;
     },
-    async login(_, { username, password }) {
+    login: async (_, { username, password }) => {
       const user = await User.findOne({ username });
       if (!user || !(await bcrypt.compare(password, user.password))) {
         return false;
@@ -113,6 +98,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-const { url } = await startStandaloneServer(server, { listen: { port: 5000 } });
-
-console.log(`🚀 Server başlatıldı... ${url}`);
+startStandaloneServer(server, { listen: { port: 5000 } }).then(({ url }) => {
+  console.log(`🚀 Server başlatıldı... ${url}`);
+});
